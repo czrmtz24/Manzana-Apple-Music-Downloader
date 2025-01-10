fa98c4ed9b575e7fd729a4618de4929ba96ed574
czrmtz24 <czrmtz24@gmail.com>
2025-01-09 18:37:50 -0800

    Update parse.py
    Modify file name sanitization function to generate macOS friendly file names

--- core/parse.py
+++ core/parse.py
@@ -8,7 +8,7 @@ def sanitize(f):
     b = ['\\', '/', ':', '*', '?', '"', '<', '>', '|', '\0']
     f = ''.join(c for c in f if c not in b)
     f = ''.join(c for c in f if 31 < ord(c))
-    f = unicodedata.normalize("NFKD", f)
+    f = unicodedata.normalize("NFKD", f).encode('ascii', 'ignore').decode('ascii')
     f = f.rstrip(". ")
     return f.strip()
 
